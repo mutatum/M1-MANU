2bf0e46a9456e25037a19072ecf4c679b0d3dfb9
DumbleDouche <www.ronan.c@gmail.com>
2024-12-08 19:50:54 +0100

    Remove test.py file and update tp4.py to add the Newton method implementation and adjust simulation parameters for improved accuracy and performance.

--- Analyse_numerique_2/tp4.py
+++ Analyse_numerique_2/tp4.py
@@ -74,10 +74,12 @@ def u0_cours(x):
     res = np.where(x < 0.3, 0.0, np.where(x < 0.7, -1.0, 0.5))
     return res
 
+
 def solution_exacte_cours(x, t):
-    if t>1:
+    if t > 1:
         raise ValueError("t must be less than 1")
-    return np.where(x<t, 1, np.where(x<=1, (1-x)/(1-t), 0))
+    return np.where(x < t, 1, np.where(x <= 1, (1 - x) / (1 - t), 0))
+
 
 def solution_td(x, t=2):
     t_etoile = 0.8
@@ -97,22 +99,39 @@ def solution_td(x, t=2):
         x < -np.sqrt(0.8 * t) + 0.7, 0, np.where(x <= 0.7 + t / 2, (x - 0.7) / t, 0.5)
     )
 
-def newton(f, df, x0, tol=1e-10, max_iter=1000):
-    x = x0
-    for i in range(max_iter):
-        x_new = x - f(x) / df(x)
-        if np.abs(x_new - x) < tol:
-            return x_new
-        x = x_new
-    raise RuntimeError("Newton's method did not converge")
+
+def newton(a, da, u0, du0, t, x, tol=1e-6, max_iter=100):
+    X = x
+
+    for _ in range(max_iter):
+        g = a(u0(X)) * t + X - x
+        gp = da(u0(X)) * du0(X) * t + 1
+        if np.abs(gp) < 1e-6:
+            print("Derivative is too small")
+            return x
+        Xnew = X - g / gp
+        if np.abs(Xnew - X) < tol:
+            return Xnew
+        X = Xnew
+    # print("Not converged", Xnew)
+
+
+def build_solution(a, da, u0, du0):
+    def u(t, x):
+        X = newton(a, da, u0, du0, t, x)
+        if X is None:
+            return None
+        return u0(X)
+
+    return u
 
 
 u0_tp = lambda x: np.sin(np.pi * 2 * x)
 
-npoints = 1000
-interval = [-3, 2.5]
-T = .6
-CFL = 0.5
+npoints = 800
+interval = [0, 1]
+T = 0.16
+CFL = 0.8
 
 x_centers, sol_LF = finite_volume_method(
     npoints, interval, u0_tp, T, flux_scheme="Lax-Friedrichs", CFL=CFL
@@ -125,14 +144,22 @@ _, sol_MR = finite_volume_method(
 )
 print(x_centers.shape, sol_LF.shape, sol_MR.shape)
 
-x_exact = np.linspace(interval[0], interval[1], npoints)
-sol_exact = solution_exacte_cours(x_exact, T)
+x_space = np.linspace(interval[0], interval[1], npoints)
+# sol_exact = solution_exacte_cours(x_exact, T)
+sol_exact = [
+    build_solution(
+        lambda u: u, lambda u: 1, lambda x: np.sin(2*np.pi*x), lambda x: 2 * np.pi * np.cos(np.pi * 2 * x)
+    )(T, x)
+    for x in x_space
+]
+print(len(sol_exact))
+plt.plot(sol_exact)
 
 plt.figure(figsize=(10, 6))
 plt.plot(x_centers, sol_LF, label="Lax-Friedrichs")
 plt.plot(x_centers, sol_LW, label="Lax-Wendroff")
 plt.plot(x_centers, sol_MR, label="Murman-Roe")
-plt.plot(x_exact, sol_exact, label="Exact Solution", linestyle="--")
+plt.plot(x_space, sol_exact, label="Exact Solution", linestyle="--")
 plt.xlabel("x")
 plt.ylabel("u")
 plt.title(
